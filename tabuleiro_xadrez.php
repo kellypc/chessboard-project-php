049c9edacc11da4c7f51043c9c5d70fdc58d8033
Kelly Cruz <kellycruz@Kellys-MacBook-Pro.local>
2023-08-15 14:39:57 -0300

    Add more pieces

--- tabuleiro_xadrez.php
+++ tabuleiro_xadrez.php
@@ -24,12 +24,61 @@
     $tabuleiro[7]['f'] = 'peão preta';
     $tabuleiro[7]['g'] = 'peão preta';
     $tabuleiro[7]['h'] = 'peão preta';
-    $tabuleiro[6];
-    $tabuleiro[5];
-    $tabuleiro[4];
-    $tabuleiro[3];
-    $tabuleiro[2];
-    $tabuleiro[1];
+
+    $tabuleiro[6]['a'] = '';
+    $tabuleiro[6]['b'] = '';
+    $tabuleiro[6]['c'] = '';
+    $tabuleiro[6]['d'] = '';
+    $tabuleiro[6]['e'] = '';
+    $tabuleiro[6]['f'] = '';
+    $tabuleiro[6]['g'] = '';
+    $tabuleiro[6]['h'] = '';
+
+    $tabuleiro[5]['a'] = '';
+    $tabuleiro[5]['b'] = '';
+    $tabuleiro[5]['c'] = '';
+    $tabuleiro[5]['d'] = '';
+    $tabuleiro[5]['e'] = '';
+    $tabuleiro[5]['f'] = '';
+    $tabuleiro[5]['g'] = '';
+    $tabuleiro[5]['h'] = '';
+
+    $tabuleiro[4]['a'] = '';
+    $tabuleiro[4]['b'] = '';
+    $tabuleiro[4]['c'] = '';
+    $tabuleiro[4]['d'] = '';
+    $tabuleiro[4]['e'] = '';
+    $tabuleiro[4]['f'] = '';
+    $tabuleiro[4]['g'] = '';
+    $tabuleiro[4]['h'] = '';
+
+    $tabuleiro[3]['a'] = '';
+    $tabuleiro[3]['b'] = '';
+    $tabuleiro[3]['c'] = '';
+    $tabuleiro[3]['d'] = '';
+    $tabuleiro[3]['e'] = '';
+    $tabuleiro[3]['f'] = '';
+    $tabuleiro[3]['g'] = '';
+    $tabuleiro[3]['h'] = '';
+
+    $tabuleiro[2]['a'] = 'peão branca';
+    $tabuleiro[2]['b'] = 'peão branca';
+    $tabuleiro[2]['c'] = 'peão branca';
+    $tabuleiro[2]['d'] = 'peão branca';
+    $tabuleiro[2]['e'] = 'peão branca';
+    $tabuleiro[2]['f'] = 'peão branca';
+    $tabuleiro[2]['g'] = 'peão branca';
+    $tabuleiro[2]['h'] = 'peão branca';
+
+    $tabuleiro[1]['a'] = 'torre branca';
+    $tabuleiro[1]['b'] = 'bispo branca';
+    $tabuleiro[1]['c'] = 'cavalo branca';
+    $tabuleiro[1]['d'] = 'rainha branca';
+    $tabuleiro[1]['e'] = 'rei branca';
+    $tabuleiro[1]['f'] = 'bispo branca';
+    $tabuleiro[1]['g'] = 'cavalo branca';
+    $tabuleiro[1]['h'] = 'torre branca';
+
 
     print_r($tabuleiro);
     print '<br />';
