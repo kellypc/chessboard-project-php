d4fade99bb90db90182257e6ae8db9c8e350a04f
Kelly Cruz <kellycruz@Kellys-MacBook-Pro.local>
2023-08-15 14:28:36 -0300

    first commit

---
+++ tabuleiro_xadrez.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tabuleiro Xadrez</title>
+</head>
+<body>
+  <?php
+    $tabuleiro[8]['a'] = 'torre preta';
+    $tabuleiro[8]['b'] = 'bispo preta';
+    $tabuleiro[8]['c'] = 'cavalo preta';
+    $tabuleiro[8]['d'] = 'rainha preta';
+    $tabuleiro[8]['e'] = 'rei preta';
+    $tabuleiro[8]['f'] = 'bispo preta';
+    $tabuleiro[8]['g'] = 'cavalo preta';
+    $tabuleiro[8]['h'] = 'torre preta';
+
+    $tabuleiro[7]['a'] = 'peão preta';
+    $tabuleiro[7]['b'] = 'peão preta';
+    $tabuleiro[7]['c'] = 'peão preta';
+    $tabuleiro[7]['d'] = 'peão preta';
+    $tabuleiro[7]['e'] = 'peão preta';
+    $tabuleiro[7]['f'] = 'peão preta';
+    $tabuleiro[7]['g'] = 'peão preta';
+    $tabuleiro[7]['h'] = 'peão preta';
+    $tabuleiro[6];
+    $tabuleiro[5];
+    $tabuleiro[4];
+    $tabuleiro[3];
+    $tabuleiro[2];
+    $tabuleiro[1];
+
+    print_r($tabuleiro);
+    print '<br />';
+    echo "<h1>ola</h1>";
+  ?>
+</body>
+</html>
